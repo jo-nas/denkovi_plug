ae765c10bf1fa771c0ffbcb1eebc7eccf1e80fca
Jonas Steinkamp <jonas@steinka.mp>
2017-06-06 18:25:52 +0200

    add tests for channel getter

--- tests/test_eight_relais.py
+++ tests/test_eight_relais.py
@@ -109,3 +109,93 @@ def test_it_deactivates_all_channels(relay):
     relay._output = 0b11111111
     relay.all = False
     assert relay._output == 0b00000000
+
+
+def test_it_can_get_true_from_channel_1(relay):
+    relay._output = 0b00000001
+    assert relay.channel_1 is True
+
+
+def test_it_can_get_false_from_channel_1(relay):
+    relay._output = 0b11111110
+    assert relay.channel_1 is False
+
+
+def test_it_can_get_true_from_channel_2(relay):
+    relay._output = 0b00000010
+    assert relay.channel_2 is True
+
+
+def test_it_can_get_false_from_channel_2(relay):
+    relay._output = 0b11111101
+    assert relay.channel_2 is False
+
+
+def test_it_can_get_true_from_channel_3(relay):
+    relay._output = 0b00000100
+    assert relay.channel_3 is True
+
+
+def test_it_can_get_false_from_channel_3(relay):
+    relay._output = 0b11111011
+    assert relay.channel_3 is False
+
+
+def test_it_can_get_true_from_channel_4(relay):
+    relay._output = 0b00001000
+    assert relay.channel_4 is True
+
+
+def test_it_can_get_false_from_channel_4(relay):
+    relay._output = 0b11110111
+    assert relay.channel_4 is False
+
+
+def test_it_can_get_true_from_channel_5(relay):
+    relay._output = 0b00010000
+    assert relay.channel_5 is True
+
+
+def test_it_can_get_false_from_channel_5(relay):
+    relay._output = 0b11101111
+    assert relay.channel_5 is False
+
+
+def test_it_can_get_true_from_channel_6(relay):
+    relay._output = 0b00100000
+    assert relay.channel_6 is True
+
+
+def test_it_can_get_false_from_channel_6(relay):
+    relay._output = 0b11011111
+    assert relay.channel_6 is False
+
+
+def test_it_can_get_true_from_channel_7(relay):
+    relay._output = 0b01000000
+    assert relay.channel_7 is True
+
+
+def test_it_can_get_false_from_channel_7(relay):
+    relay._output = 0b10111111
+    assert relay.channel_7 is False
+
+
+def test_it_can_get_true_from_channel_8(relay):
+    relay._output = 0b10000000
+    assert relay.channel_8 is True
+
+
+def test_it_can_get_false_from_channel_8(relay):
+    relay._output = 0b01111111
+    assert relay.channel_8 is False
+
+
+def test_it_can_get_true_from_all_channels(relay):
+    relay._output = 0b11111111
+    assert relay.all == 0b11111111
+
+
+def test_it_can_get_false_from_all_channels(relay):
+    relay._output = 0b00000000
+    assert relay.all == 0b00000000
