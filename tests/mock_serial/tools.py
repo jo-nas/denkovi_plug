76d4ac06e049c483bf744bc076076d0168c2dc31
Jonas Steinkamp <jonas@steinka.mp>
2017-09-26 11:55:34 +0200

    fix the tests

--- tests/mock_serial/tools.py
+++ tests/mock_serial/tools.py
@@ -2,6 +2,6 @@ class list_ports:
     @staticmethod
     def comports():
         class data:
-            serial_number = "DAE002OHA"
+            serial_number = "DAE002oH"
             device = ""
         return (data(),)
